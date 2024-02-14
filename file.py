0a3ca0811b3050240eb36a0ec9389fb362c36bbb
jmailnaif <jamailotb15@gmail.com>
2024-02-14 07:57:45 +0300

    lab

---
+++ file.py
@@ -0,0 +1,20 @@
+#import os
+#os.mkdir("to-do list")
+while True:
+    def input_user:
+input(" do yeo want to add a new to-do item ? answer by 'y' for yes or 'n' for no :")
+if user_in.lower()=="y":
+user_in = ("write your task :")
+file = open("to-do.txt, mode ="r", encoding ="utf-8")
+            to-do=
+            file.read()
+            print(to_do)
+elif 
+    user_in.lower()=="n":
+    print("goood")
+    break 
+    if user_in.lower() == "exit":
+    print("thank you for using the to-do program, com bak again soon")
+    break
+    print("some thing want wrong")
+input_user()    
